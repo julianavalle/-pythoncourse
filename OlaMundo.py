8a040cc1d7bf3b4821c55e74048e270af9bedd38
Juliana <juju.pvg@gmail.com>
2020-03-16 20:41:42 -0300

    Estruturas Condicionais

--- OlaMundo.py
+++ OlaMundo.py
@@ -1,5 +1,6 @@
-print("Ola Mundo")
+print("Olá Mundo\n")
 
+# ***************************************** VARIÁVEIS *****************************************
 var1 = 1 #Variável Inteira
 var2 = 1.1 #Variável Float
 var3 = "String" #Variável String
@@ -8,4 +9,18 @@ var4 = True #Variável Booleana
 print(var1, "- Variável Inteira")
 print(var2, "- Variável Float")
 print(var3, "- Variável String")
-print(var4, "- Variável Booleana")
+print(var4, "- Variável Booleana\n")
+
+# *********************************** ESTRUTURAS CONDICIONAIS ***********************************
+x = 1
+y = 2
+
+if x == y:
+    print("Números iguais")
+elif x < y:
+    print("x menor que y")
+elif y > x:
+    print("y maior que x")
+else:
+    print("Números diferentes")
+    
