400831e74b49fa2134458a0aecf109d5ccf5777f
Juliana <juju.pvg@gmail.com>
2020-03-16 20:43:39 -0300

    Estruturas de Repetição - For

--- OlaMundo.py
+++ OlaMundo.py
@@ -16,11 +16,30 @@ x = 1
 y = 2
 
 if x == y:
-    print("Números iguais")
+    print("Números iguais\n")
 elif x < y:
-    print("x menor que y")
+    print("x menor que y\n")
 elif y > x:
-    print("y maior que x")
+    print("y maior que x\n")
 else:
-    print("Números diferentes")
+    print("Números diferentes\n")
+
+# ******************************* ESTRUTURAS DE REPETIÇÃO: FOR *******************************
+lista1 = [1, 3, 5, 7, 9]
+lista2 = ["olá", "mundo", "!"]
+lista3 = [1, "prato", "de", "trigo", "para", 1, "tigre", "triste", 0.0]
+
+print("Lista 1:")
+for i in lista1:
+    print(i)
+print("\n")
+
+print("Lista 2:")
+for i in lista2:
+    print(i)
+print("\n")
+
+print("Lista 3:")
+for i in lista3:
+    print(i)
     
