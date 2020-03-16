799d22751db79721269b49e2a29afe21dfbda192
Juliana <juju.pvg@gmail.com>
2020-03-16 19:52:10 -0300

    Adicionando Variáveis

--- OlaMundo.py
+++ OlaMundo.py
@@ -1,1 +1,11 @@
 print("Ola Mundo")
+
+var1 = 1 #Variável Inteira
+var2 = 1.1 #Variável Float
+var3 = "String" #Variável String
+var4 = True #Variável Booleana
+
+print(var1, "- Variável Inteira")
+print(var2, "- Variável Float")
+print(var3, "- Variável String")
+print(var4, "- Variável Booleana")
